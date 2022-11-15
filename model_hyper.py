c23907a8d85f4853d4c3a1750d302dc41f5803bc
xxalison <109158990+xxalisonn@users.noreply.github.com>
2022-11-15 11:34:28 +0800

    Update model_hyper.py

--- model_hyper.py
+++ model_hyper.py
@@ -3,7 +3,8 @@ from hyper_embedding import *
 from collections import OrderedDict
 import torch.nn.functional as F
 import torch
-
+from torch.nn.modules.conv import Conv1d, Conv2d
+from torch.nn.modules.activation import ReLU
 
 class RelationMetaLearner(nn.Module):
     def __init__(self, few, embed_size=100, num_hidden1=500, num_hidden2=200, out_size=100, dropout_p=0.5):
@@ -41,69 +42,88 @@ class RelationMetaLearner(nn.Module):
 
         return x.view(size[0], 1, 1, self.out_size)
 
+class AttentionMatcher(nn.Module):
+    def __init__(self,embed_dim):
+        super(AttentionMatcher, self).__init__()
+        # attn_mat = nn.Parameter(torch.randn(embed_dim))
+        self.gate_w = nn.Linear(embed_dim,1)
+        self.gate_b = nn.Parameter(torch.FloatTensor(1))
 
-# class EmbeddingLearner(nn.Module):
-#     def __init__(self):
-#         super(EmbeddingLearner, self).__init__()
+    def forward(self, M, N):
+        M = torch.squeeze(M)
+        N = torch.squeeze(N)
+        # calculate the similarity score(mask the attn value of ground truth relation)
+        mt = torch.matmul(N, M.transpose(0, 1))
+        diag = mt - torch.diag(torch.diag(mt))
+        attn_weight = torch.softmax(diag, dim=-1)
+        # multiply the score with relation prototype
+        out_attn = torch.matmul(attn_weight, M)
+        # set a gate for fusion(0<=gate<=1)
+        gate_tmp = self.gate_w(out_attn) + self.gate_b
+        gate = torch.sigmoid(gate_tmp)
+        # gate*boosted + (1-gate)*original
+        out_rel = torch.mul(out_attn,gate)
+        boosted = out_rel + torch.mul(N,1.0-gate)
 
-#     def projected(self, ent, norm):
-#         norm = F.normalize(norm, p=2, dim=-1)
-#         return ent - torch.sum(ent * norm, dim = 1, keepdim=True) * norm
+        return boosted.unsqueeze(1).unsqueeze(1)
 
-#     def forward(self, h, t, r, norm, pos_num):
-#         norm = norm[:,:1,:,:]						# revise
-#         h = self.projected(h,norm)
-#         t = self.projected(t,norm)
-#         score = -torch.norm(h + r - t, 2, -1).squeeze(2)
-#         p_score = score[:, :pos_num]
-#         n_score = score[:, pos_num:]
-#         return p_score, n_score
+class PatternLearner(nn.Module):
+    def __init__(self, input_channels, out_channels=[128, 64, 32, 1]):
+        super(PatternLearner, self).__init__()
+        self.out_channels = out_channels
+        self.input_channles = input_channels
+        self.encoder = nn.Sequential(
+            OrderedDict(
+                [
+                    (
+                        "conv1",
+                        Conv2d(self.input_channles, self.out_channels[0], (1, 3)),
+                    ),
+                    # BatchNorm2d(128),
+                    ("relu1", ReLU()),
+                    (
+                        "conv2",
+                        Conv2d(self.out_channels[0], self.out_channels[1], (1, 1)),
+                    ),
+                    # BatchNorm2d(64),
+                    ("relu2", ReLU()),
+                    (
+                        "conv3",
+                        Conv2d(self.out_channels[1], self.out_channels[2], (1, 1)),
+                    ),
+                    # BatchNorm2d(32),
+                    ("relu3", ReLU()),
+                    (
+                        "conv4",
+                        Conv2d(self.out_channels[2], self.out_channels[3], (1, 1)),
+                    ),
+                ]
+            )
+        )
+
+        for name, param in self.encoder.named_parameters():
+            if "conv" and "weight" in name:
+                torch.nn.init.kaiming_normal_(param)
+
+    def forward(self, x):
+        batch_size, num_triples, num_channels, input_length, dim = x.size()
+        x = x.view(batch_size * num_triples, num_channels, input_length, dim).transpose(
+            2, 3
+        )
+        x = self.encoder(x)
+        return x.view(batch_size, num_triples, -1)
 
 class EmbeddingLearner(nn.Module):
     def __init__(self):
         super(EmbeddingLearner, self).__init__()
 
-    def forward(self, h, t, r, pos_num):
+    def forward(self, h, t, r,pos_num):					# revise
         score = -torch.norm(h + r - t, 2, -1).squeeze(2)
         p_score = score[:, :pos_num]
         n_score = score[:, pos_num:]
         return p_score, n_score
 
-class LSTM_attn(nn.Module):
-    def __init__(self, embed_size=100, n_hidden=200, out_size=100, layers=1, dropout=0.5):
-        super(LSTM_attn, self).__init__()
-        self.embed_size = embed_size
-        self.n_hidden = n_hidden
-        self.out_size = out_size
-        self.layers = layers
-        self.dropout = dropout
-        self.lstm = nn.LSTM(self.embed_size*2, self.n_hidden, self.layers, bidirectional=True, dropout=self.dropout)
-        #self.gru = nn.GRU(self.embed_size*2, self.n_hidden, self.layers, bidirectional=True)
-        self.out = nn.Linear(self.n_hidden*2*self.layers, self.out_size)
 
-    def attention_net(self, lstm_output, final_state):
-        hidden = final_state.view(-1, self.n_hidden*2, self.layers)
-        attn_weight = torch.bmm(lstm_output, hidden).squeeze(2).cuda()
-        #batchnorm = nn.BatchNorm1d(5, affine=False).cuda()
-        #attn_weight = batchnorm(attn_weight)
-        soft_attn_weight = F.softmax(attn_weight, 1)
-        context = torch.bmm(lstm_output.transpose(1,2), soft_attn_weight)
-        context = context.view(-1, self.n_hidden*2*self.layers)
-        return context
-
-    def forward(self, inputs):
-        size = inputs.shape
-        inputs = inputs.contiguous().view(size[0], size[1], -1)
-        input = inputs.permute(1, 0, 2)
-        hidden_state = Variable(torch.zeros(self.layers*2, size[0], self.n_hidden)).cuda()
-        cell_state = Variable(torch.zeros(self.layers*2, size[0], self.n_hidden)).cuda()
-        output, (final_hidden_state, final_cell_state) = self.lstm(input, (hidden_state, cell_state))  # LSTM
-        output = output.permute(1, 0, 2)
-        attn_output = self.attention_net(output, final_cell_state)      # change log
-
-        outputs = self.out(attn_output)
-        return outputs.view(size[0], 1, 1, self.out_size)    
-    
 class MetaR(nn.Module):
     def __init__(self, dataset, parameter):
         super(MetaR, self).__init__()
@@ -114,17 +134,20 @@ class MetaR(nn.Module):
         self.margin = parameter['margin']
         self.abla = parameter['ablation']
         self.embedding = Embedding(dataset, parameter)
+        self.pattern_learner = PatternLearner(input_channels=1)
+        self.attention_matcher = AttentionMatcher(self.embed_dim)
+        self.relation_prototype = dict()
+        self.rel_q_sharing = dict()
 
         if parameter['dataset'] == 'Wiki-One':
-            self.relation_learner = LSTM_attn(embed_size=50, n_hidden=100, out_size=50,layers=2, dropout=0.5)
+            self.relation_learner = RelationMetaLearner(parameter['few'], embed_size=50, num_hidden1=250,
+                                                        num_hidden2=100, out_size=50, dropout_p=self.dropout_p)
         elif parameter['dataset'] == 'NELL-One':
-            self.relation_learner = LSTM_attn(embed_size=100, n_hidden=450, out_size=100, layers=2, dropout=self.dropout_p)
-            
-        self.h_embedding = H_Embedding(dataset, parameter)
+            self.relation_learner = RelationMetaLearner(parameter['few'], embed_size=100, num_hidden1=500,
+                                                        num_hidden2=200, out_size=100, dropout_p=self.dropout_p)
+
         self.embedding_learner = EmbeddingLearner()
         self.loss_func = nn.MarginRankingLoss(self.margin)
-        self.rel_q_sharing = dict()
-        self.h_norm = None
 
     def split_concat(self, positive, negative):
         pos_neg_e1 = torch.cat([positive[:, :, 0, :],
@@ -133,17 +156,39 @@ class MetaR(nn.Module):
                                 negative[:, :, 1, :]], 1).unsqueeze(2)
         return pos_neg_e1, pos_neg_e2
 
+    def concat_relation(self, pairs, relation):
+        if relation.shape[1] != pairs.shape[1]:
+            relation = relation.repeat(1, pairs.shape[1], 1, 1, 1)
+        triplet = torch.cat((relation, pairs), dim=-2)
+        return triplet[:, :, :, [1, 0, 2], :]
+
+    def get_relation(self, pairs, mean=False):
+        """return (tail-head)"""
+        relation = (pairs[:, :, :, 1, :] - pairs[:, :, :, 0, :]).unsqueeze(-2)
+        if mean:
+            relation = torch.mean(relation, dim=1, keepdim=True)
+        return relation
+
     def forward(self, task, iseval=False, curr_rel=''):
         # transfer task string into embedding
         support, support_negative, query, negative = [self.embedding(t) for t in task]
-#         norm_vector = self.h_embedding(task[0])
         few = support.shape[1]              # num of few
         num_sn = support_negative.shape[1]  # num of support negative
         num_q = query.shape[1]              # num of query
         num_n = negative.shape[1]           # num of query negative
 
-        rel = self.relation_learner(support)
+        pos_relation = self.get_relation(support.unsqueeze(2), mean=True)
+        support_concat = self.concat_relation(support.unsqueeze(2), pos_relation)
+
+        # support.squeeze(2)
+        # rel = self.relation_learner(support)
+
+        rel = self.pattern_learner(support_concat).unsqueeze(2)
+        rel = torch.mean(rel,dim=1,keepdim=True)
+        rel = self.attention_matcher(rel,pos_relation.squeeze(2))
+
         rel.retain_grad()
+
 
         # relation for support
         rel_s = rel.expand(-1, few+num_sn, -1, -1)
@@ -157,10 +202,8 @@ class MetaR(nn.Module):
                 # split on e1/e2 and concat on pos/neg
                 sup_neg_e1, sup_neg_e2 = self.split_concat(support, support_negative)
 
-#                 p_score, n_score = self.embedding_learner(sup_neg_e1, sup_neg_e2, rel_s, norm_vector,few)
-                p_score, n_score = self.embedding_learner(sup_neg_e1, sup_neg_e2, rel_s, few)
+                p_score, n_score = self.embedding_learner(sup_neg_e1, sup_neg_e2, rel_s,few)
 
-                # y = torch.Tensor([1]).to(self.device)
                 y = torch.ones(p_score.size()).cuda()
                 self.zero_grad()
                 loss = self.loss_func(p_score, n_score, y)
@@ -168,22 +211,15 @@ class MetaR(nn.Module):
 
                 grad_meta = rel.grad
                 rel_q = rel - self.beta*grad_meta
-                norm_q = norm_vector - self.beta*grad_meta
             else:
                 rel_q = rel
-                norm_q = norm_vector
 
             self.rel_q_sharing[curr_rel] = rel_q
-            self.h_norm = norm_vector.mean(0)
-            self.h_norm = self.h_norm.unsqueeze(0)
 
         rel_q = rel_q.expand(-1, num_q + num_n, -1, -1)
-                
-        if iseval:
-            norm_q = self.h_norm
 
         que_neg_e1, que_neg_e2 = self.split_concat(query, negative)  # [bs, nq+nn, 1, es]
-#         p_score, n_score = self.embedding_learner(que_neg_e1, que_neg_e2, rel_q, norm_q, num_q)
         p_score, n_score = self.embedding_learner(que_neg_e1, que_neg_e2, rel_q, num_q)
 
         return p_score, n_score
+
